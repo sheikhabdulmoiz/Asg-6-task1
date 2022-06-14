e015eea76510382155b4c51d77f3803ebfdf35ef
Abdul Moiz <abdulmoiz15000@gmail.com>
2022-06-14 22:03:57 +0500

    Search by forloop and indexOf

---
+++ app.js
@@ -0,0 +1,50 @@
+
+// ---------------------------------------- Task # 01 (by for loop)-------------------------------------------------------
+
+
+// var foods=["biryani","pulao","mutton","chickentikka","Haleem","Karahi","Sajji",""];
+// var select=prompt("Enter food item you want?");
+// var userFood = select.toLowerCase();      //Return karta hai
+// var otherItems=["Drinks ","Dessert ","Shakes ","Chinese ","Thai food ","Fish "];
+// var flag=false;
+// for(var i=0 ; i<foods.length;i++){
+//     if(userFood===foods[i]){
+//         alert(select+" is available.");
+//         for(var j=0;j<otherItems.length;j++){
+//         document.write(otherItems[j]);
+//     }
+//     flag=true;
+//     }
+// }
+// if(flag===false){
+//     alert(select+" is not available")
+// }
+
+// ---------------------------------------- Task # 02 (by index.Of) -------------------------------------------------------
+
+
+// var foods=["biryani","pulao","mutton","chickentikka","haleem","karahi","sajji","daal"];
+// // var otherItems=["Drinks ","Dessert ","Shakes ","Chinese ","Thai food ","Fish "];
+// var select=prompt("Enter food item you want?");
+// var userFood = select.toLowerCase();      //Return karta hai
+// var result=foods.indexOf(userFood);
+// // console.log(result);
+// var get=foods.slice(result,result+1);
+//  var finalGet= get.toString();
+// // console.log(get)
+// if(finalGet===""){
+//     alert(select+" is not available")
+// }
+// else{
+//     alert(get+" is available")
+    
+// }
+
+
+
+
+
+
+
+
+
